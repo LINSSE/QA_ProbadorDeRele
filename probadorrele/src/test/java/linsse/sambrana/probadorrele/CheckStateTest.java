07e505a9f4c025f626d418c1d9bf6be57f5991bc
Ivan Sambrana <sambranaivan@gmail.com>
2017-10-06 17:53:00 -0300

    renombro test

--- probadorrele/src/test/java/linsse/sambrana/probadorrele/CheckStateTest.java
+++ probadorrele/src/test/java/linsse/sambrana/probadorrele/CheckStateTest.java
@@ -4,7 +4,7 @@ import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Test;
 
-public class TestEntradas {
+public class CheckStateTest {
 
 	private ManejadorEntradas m = new ManejadorEntradas();
 
